e577005442c2284da6968804aaf539ffd342c101
jgyuan1 <jgyuan1@163.com>
2022-08-05 14:35:04 -0700

    Initial Commit

---
+++ MemorizeCards/MemorizeCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeCardsApp.swift
+//  MemorizeCards
+//
+//  Created by CHENGLONG HAO on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
